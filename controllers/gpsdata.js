4c4a43bfb058b01db90d69347819e59dfd140925
jayesh <jayesh@bugzstudio.com>
2017-07-19 07:33:42 +0000

    git-svn-id: https://svn.bugzstudio.com:7777/svn/GPScannerAPI@15 c13d9488-3209-ee48-8958-08fe834ea18d

--- controllers/gpsdata.js
+++ controllers/gpsdata.js
@@ -11,7 +11,6 @@ router.get('/GetAllGpsData', function(req, res) {
     var objSearch = objParam.search;
     var Orderby = objColumns[parseInt(objOrderBy[0].column)].data + ' ' + objOrderBy[0].dir;
     var search = {};
-    var search1 = {};
     if (objSearch != null && objSearch != '') {
         search['$or'] = [];
         for (var i = 0; i < objColumns.length; i++) {
@@ -168,6 +167,368 @@ router.get('/GetAllGpsDevice', function(req, res) {
 });
 
 
+router.get('/ExportAllGpsData', function(req, res) {
+    var conf = {};
+    conf.name = "Sheet1";
+    conf.cols = [{
+            caption: 'DeviceId',
+            type: 'string'
+        }, {
+            caption: 'Datetime',
+            type: 'string'
+        }, {
+            caption: 'Latitude',
+            type: 'string'
+        }, {
+            caption: 'Longtitude',
+            type: 'string'
+        }, {
+            caption: 'GPSPositioning',
+            type: 'string'
+        }, {
+            caption: 'Speed',
+            type: 'string'
+        }, {
+            caption: 'Direction',
+            type: 'string'
+        }, {
+            caption: 'Status',
+            type: 'string'
+        }, {
+            caption: 'IsRelayToStopTheCar',
+            type: 'string'
+        }, {
+            caption: 'IsSirenSound',
+            type: 'string'
+        }, {
+            caption: 'IsLockTheDoor',
+            type: 'string'
+        }, {
+            caption: 'IsUnlockTheDoor',
+            type: 'string'
+        }, {
+            caption: 'IsSOS',
+            type: 'string'
+        }, {
+            caption: 'IsDoor',
+            type: 'string'
+        }, {
+            caption: 'IsEngine',
+            type: 'string'
+        }, {
+            caption: 'CreatedDate',
+            type: 'string'
+        }, {
+            caption: 'Altitude',
+            type: 'string'
+        }, {
+            caption: 'AD1',
+            type: 'string'
+        },
+        {
+            caption: 'AD2',
+            type: 'string'
+        }, {
+            caption: 'OdoMeter',
+            type: 'string'
+        }
+    ];
+
+
+
+    var objParam = req.query;
+    var Orderby = 'CreatedDate asc';
+    var search = {};
+
+
+    search['$and'] = [];
+    var DeviceId = objParam.DeviceId;
+    if (DeviceId != null && DeviceId != '' && DeviceId != undefined) {
+        var obj = new Object();
+        obj['DeviceId'] = {
+            $eq: DeviceId
+        };
+        search['$and'].push(obj);
+    }
+    var StartDate = objParam.StartDate;
+    var EndDate = objParam.EndDate;
+    if (StartDate != '' && EndDate != '' && StartDate != undefined && EndDate != undefined) {
+        StartDate = convertdateformat(StartDate, 0);
+        EndDate = convertdateformat(EndDate, 1);
+        var obj = new Object();
+        obj['CreatedDate'] = {
+            $between: [StartDate, EndDate]
+        };
+        search['$and'].push(obj);
+    } else if (StartDate != null && StartDate != '' && StartDate != undefined) {
+        StartDate = convertdateformat(StartDate, 0);
+        var obj = new Object();
+        obj['CreatedDate'] = {
+            $gt: StartDate
+        };
+        search['$and'].push(obj);
+    } else if (EndDate != null && EndDate != '' && EndDate != undefined) {
+        EndDate = convertdateformat(EndDate, 1);
+        var obj = new Object();
+        obj['CreatedDate'] = {
+            $lt: EndDate
+        };
+        search['$and'].push(obj);
+    }
+
+    Gps.findAll({
+        where: search,
+        order: Orderby,
+    }).then(function(response) {
+        conf.rows = [];
+        var DeviceId = '';
+        var Datetime = '';
+        var Latitude = '';
+        var Longtitude = '';
+        var GPSPositioning = '';
+        var Speed = '';
+        var Direction = '';
+        var Status = '';
+        var IsRelayToStopTheCar = false;
+        var IsSirenSound = false;
+        var IsLockTheDoor = false;
+        var IsUnlockTheDoor = false;
+        var IsSOS = false;
+        var IsDoor = false;
+        var IsEngine = false;
+        var CreatedDate = '';
+        var Altitude = '';
+        var AD1 = '';
+        var AD2 = '';
+        var OdoMeter = '';
+        GetGpsData(0);
+
+        function GetGpsData(i) {
+            if (i < response.length) {
+                var row = [];
+                if (response[i].DeviceId != null && response[i].DeviceId != '' && response[i].DeviceId != undefined) {
+                    DeviceId = response[i].DeviceId;
+                }
+
+                if (response[i].Datetime != null && response[i].Datetime != '' && response[i].Datetime != undefined) {
+                    Datetime = convertdateformat(response[i].Datetime, 2);
+                }
+
+                if (response[i].Latitude != null && response[i].Latitude != '' && response[i].Latitude != undefined) {
+                    Latitude = response[i].Latitude
+                }
+
+                if (response[i].Longtitude != null && response[i].Longtitude != '' && response[i].Longtitude != undefined) {
+                    Longtitude = response[i].Longtitude;
+                }
+
+                if (response[i].GPSPositioning != null && response[i].GPSPositioning != '' && response[i].GPSPositioning != undefined) {
+                    GPSPositioning = response[i].GPSPositioning;
+                }
+
+                if (response[i].Speed != null && response[i].Speed != '' && response[i].Speed != undefined) {
+                    Speed = response[i].Speed;
+                }
+
+                if (response[i].Direction != null && response[i].Direction != '' && response[i].Direction != undefined) {
+                    Direction = response[i].Direction;
+                }
+
+                if (response[i].Status != null && response[i].Status != '' && response[i].Status != undefined) {
+                    Status = response[i].Status;
+                }
+
+                if (response[i].IsRelayToStopTheCar != null && response[i].IsRelayToStopTheCar != '' && response[i].IsRelayToStopTheCar != undefined) {
+                    IsRelayToStopTheCar = response[i].IsRelayToStopTheCar;
+                }
+                if (response[i].IsSirenSound != null && response[i].IsSirenSound != '' && response[i].IsSirenSound != undefined) {
+                    IsSirenSound = response[i].IsSirenSound;
+                }
+                if (response[i].IsLockTheDoor != null && response[i].IsLockTheDoor != '' && response[i].IsLockTheDoor != undefined) {
+                    IsLockTheDoor = response[i].IsLockTheDoor;
+                }
+                if (response[i].IsUnlockTheDoor != null && response[i].IsUnlockTheDoor != '' && response[i].IsUnlockTheDoor != undefined) {
+                    IsUnlockTheDoor = response[i].IsUnlockTheDoor;
+                }
+                if (response[i].IsSOS != null && response[i].IsSOS != '' && response[i].IsSOS != undefined) {
+                    IsSOS = response[i].IsSOS;
+                }
+                if (response[i].IsDoor != null && response[i].IsDoor != '' && response[i].IsDoor != undefined) {
+                    IsDoor = response[i].IsDoor;
+                }
+                if (response[i].IsEngine != null && response[i].IsEngine != '' && response[i].IsEngine != undefined) {
+                    IsEngine = response[i].IsEngine;
+                }
+                if (response[i].CreatedDate != null && response[i].CreatedDate != '' && response[i].CreatedDate != undefined) {
+                    CreatedDate = convertdateformat(response[i].CreatedDate, 2);
+                }
+                if (response[i].Altitude != null && response[i].Altitude != '' && response[i].Altitude != undefined) {
+                    Altitude = response[i].Altitude;
+                }
+                if (response[i].AD1 != null && response[i].AD1 != '' && response[i].AD1 != undefined) {
+                    AD1 = response[i].AD1;
+                }
+                if (response[i].AD2 != null && response[i].AD2 != '' && response[i].AD2 != undefined) {
+                    AD2 = response[i].AD2;
+                }
+                if (response[i].OdoMeter != null && response[i].OdoMeter != '' && response[i].OdoMeter != undefined) {
+                    OdoMeter = response[i].OdoMeter;
+                }
+                row.push(DeviceId, Datetime, Latitude, Longtitude, GPSPositioning, Speed, Direction, Status, IsRelayToStopTheCar, IsSirenSound, IsLockTheDoor, IsUnlockTheDoor, IsSOS, IsDoor, IsEngine, CreatedDate, Altitude, AD1, AD2, OdoMeter);
+                conf.rows.push(row);
+                GetGpsData(i + 1);
+            } else {
+                var result = nodeExcel.execute(conf);
+                res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+                res.setHeader("Content-Disposition", "attachment; filename=GpdData.xlsx");
+                res.end(result, 'binary');
+            }
+
+        }
+    });
+});
+
+router.get('/ExportAlarm', function(req, res) {
+    var conf = {};
+    conf.name = "Sheet1";
+    conf.cols = [{
+        caption: 'AlarmCode',
+        type: 'string'
+    }, {
+        caption: 'DeviceId',
+        type: 'string'
+    }, {
+        caption: 'Latitude',
+        type: 'string'
+    }, {
+        caption: 'Longtitude',
+        type: 'string'
+    }, {
+        caption: 'GPSPositioning',
+        type: 'string'
+    }, {
+        caption: 'Status',
+        type: 'string'
+    }, {
+        caption: 'CreatedDate',
+        type: 'string'
+    }];
+
+
+
+    var objParam = req.query;
+    var Orderby = 'CreatedDate asc';
+    var search = {};
+
+
+    search['$and'] = [];
+    var DeviceId = objParam.DeviceId;
+    if (DeviceId != null && DeviceId != '' && DeviceId != undefined) {
+        var obj = new Object();
+        obj['DeviceId'] = {
+            $eq: DeviceId
+        };
+        search['$and'].push(obj);
+    }
+    var StartDate = objParam.StartDate;
+    var EndDate = objParam.EndDate;
+    if (StartDate != '' && EndDate != '' && StartDate != undefined && EndDate != undefined) {
+        StartDate = convertdateformat(StartDate, 0);
+        EndDate = convertdateformat(EndDate, 1);
+        var obj = new Object();
+        obj['CreatedDate'] = {
+            $between: [StartDate, EndDate]
+        };
+        search['$and'].push(obj);
+    } else if (StartDate != null && StartDate != '' && StartDate != undefined) {
+        StartDate = convertdateformat(StartDate, 0);
+        var obj = new Object();
+        obj['CreatedDate'] = {
+            $gt: StartDate
+        };
+        search['$and'].push(obj);
+    } else if (EndDate != null && EndDate != '' && EndDate != undefined) {
+        EndDate = convertdateformat(EndDate, 1);
+        var obj = new Object();
+        obj['CreatedDate'] = {
+            $lt: EndDate
+        };
+        search['$and'].push(obj);
+    }
+    var AlarmCode = objParam.AlarmCode;
+
+    if (AlarmCode != null && AlarmCode != undefined && AlarmCode != '') {
+        var obj = new Object();
+        obj['AlarmCode'] = {
+            $eq: AlarmCode
+        };
+        search['$and'].push(obj);
+    }
+
+    Alarm.findAll({
+        where: search,
+        order: Orderby,
+    }).then(function(response) {
+        conf.rows = [];
+        var AlarmCode = '';
+        var DeviceId = '';
+        var Latitude = '';
+        var Longtitude = '';
+        var GPSPositioning = '';
+        var Status = '';
+        var CreatedDate = '';
+        GetAlarmData(0);
+
+        function GetAlarmData(i) {
+            if (i < response.length) {
+                var row = [];
+                if (response[i].AlarmCode != null && response[i].AlarmCode != '' && response[i].AlarmCode != undefined) {
+                    var list = AlarmCodedata();
+                    var obj = u.findWhere(list, { AlarmCode: response[i].AlarmCode });
+                    if (obj != null && obj != undefined && obj != '') {
+                        AlarmCode = obj.Alarm;
+                    }
+                }
+                if (response[i].DeviceId != null && response[i].DeviceId != '' && response[i].DeviceId != undefined) {
+                    DeviceId = response[i].DeviceId;
+                }
+
+                if (response[i].CreatedDate != null && response[i].CreatedDate != '' && response[i].CreatedDate != undefined) {
+                    CreatedDate = convertdateformat(response[i].CreatedDate, 2);
+                }
+
+                if (response[i].Latitude != null && response[i].Latitude != '' && response[i].Latitude != undefined) {
+                    Latitude = response[i].Latitude
+                }
+
+                if (response[i].Longtitude != null && response[i].Longtitude != '' && response[i].Longtitude != undefined) {
+                    Longtitude = response[i].Longtitude;
+                }
+
+                if (response[i].GPSPositioning != null && response[i].GPSPositioning != '' && response[i].GPSPositioning != undefined) {
+                    GPSPositioning = response[i].GPSPositioning;
+                }
+
+                if (response[i].Status != null && response[i].Status != '' && response[i].Status != undefined) {
+                    Status = response[i].Status;
+                }
+
+
+                row.push(AlarmCode, DeviceId, Latitude, Longtitude, GPSPositioning, Status, CreatedDate);
+                conf.rows.push(row);
+                GetAlarmData(i + 1);
+            } else {
+                var result = nodeExcel.execute(conf);
+                res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+                res.setHeader("Content-Disposition", "attachment; filename=Alarm.xlsx");
+                res.end(result, 'binary');
+            }
+
+        }
+    });
+});
+
+
 function convertdateformat(date1, flg) {
     var date = new Date(date1);
     var firstdayMonth = date.getMonth() + 1;
@@ -180,10 +541,83 @@ function convertdateformat(date1, flg) {
         return ("0000" + firstdayYear.toString()).slice(-4) + "-" + ("00" + firstdayMonth.toString()).slice(-2) + "-" + ("00" + firstdayDay.toString()).slice(-2) + " " + "23:59:59";
     } else if (flg == "Excel Export") {
         return ("0000" + firstdayYear.toString()).slice(-4) + "-" + ("00" + firstdayMonth.toString()).slice(-2) + "-" + ("00" + firstdayDay.toString()).slice(-2) + " " + ("00" + firstdayHours.toString()).slice(-2) + ':' + ("00" + firstdayMinutes.toString()).slice(-2) + ':' + ("00" + firstdaySeconds.toString()).slice(-2);
+    } else if (flg == 2) {
+        return ("00" + firstdayDay.toString()).slice(-2) + "-" + ("00" + firstdayMonth.toString()).slice(-2) + "-" + ("0000" + firstdayYear.toString()).slice(-4);
     } else {
         //return ("0000" + firstdayYear.toString()).slice(-4) + "-" + ("00" + firstdayMonth.toString()).slice(-2) + "-" + ("00" + firstdayDay.toString()).slice(-2) + " " + "00:00:00";
         return ("0000" + firstdayYear.toString()).slice(-4) + "-" + ("00" + firstdayMonth.toString()).slice(-2) + "-" + ("00" + firstdayDay.toString()).slice(-2);
     }
+}
+
+
+function AlarmCodedata() {
+    var ResponseAlarm = [];
+    var obj = new Object();
+    obj.AlarmCode = "01";
+    obj.Alarm = "SOS alarm(IN1)";
+    ResponseAlarm.push(obj);
+    var obj = new Object();
+    obj.AlarmCode = "02";
+    obj.Alarm = "Line broken alarm(IN2)";
+    ResponseAlarm.push(obj);
+    var obj = new Object();
+    obj.AlarmCode = "03";
+    obj.Alarm = "Door open alarm(IN3)";
+    ResponseAlarm.push(obj);
+    var obj = new Object();
+    obj.AlarmCode = "04";
+    obj.Alarm = "Engine on alarm(IN4)";
+    ResponseAlarm.push(obj);
+    var obj = new Object();
+    obj.AlarmCode = "05";
+    obj.Alarm = "Original triggering alarm(IN5)";
+    ResponseAlarm.push(obj);
+    var obj = new Object();
+    obj.AlarmCode = "10";
+    obj.Alarm = "Law battery alarm";
+    ResponseAlarm.push(obj);
+    var obj = new Object();
+    obj.AlarmCode = "11";
+    obj.Alarm = "Over speed alarm";
+    ResponseAlarm.push(obj);
+    var obj = new Object();
+    obj.AlarmCode = "12";
+    obj.Alarm = "Movment alarm";
+    ResponseAlarm.push(obj);
+    var obj = new Object();
+    obj.AlarmCode = "13";
+    obj.Alarm = "Geo-fence alarm";
+    ResponseAlarm.push(obj);
+    var obj = new Object();
+    obj.AlarmCode = "30";
+    obj.Alarm = "Vibration alarm";
+    ResponseAlarm.push(obj);
+    var obj = new Object();
+    obj.AlarmCode = "50";
+    obj.Alarm = "External power cut alarm";
+    ResponseAlarm.push(obj);
+    var obj = new Object();
+    obj.AlarmCode = "52";
+    obj.Alarm = "Veer report";
+    ResponseAlarm.push(obj);
+    var obj = new Object();
+    obj.AlarmCode = "60";
+    obj.Alarm = "Fatigue driving alarm";
+    ResponseAlarm.push(obj);
+    var obj = new Object();
+    obj.AlarmCode = "71";
+    obj.Alarm = "Crash alarm(Harsh brake)";
+    ResponseAlarm.push(obj);
+    var obj = new Object();
+    obj.AlarmCode = "71";
+    obj.Alarm = "Acceleration alarm";
+    ResponseAlarm.push(obj);
+    var obj = new Object();
+    obj.AlarmCode = "81";
+    obj.Alarm = "Fuel loss alarm";
+    ResponseAlarm.push(obj);
+    return ResponseAlarm;
+
 }
 
 module.exports = router;
