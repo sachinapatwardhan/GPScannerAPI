ec75d0ddb45eacde4dd7de4313f14f4b354959da
jaimin <jaimin@bugzstudio.com>
2017-08-02 12:11:12 +0000

    set Longtitude to Longitude
    git-svn-id: https://svn.bugzstudio.com:7777/svn/GPScannerAPI@64 c13d9488-3209-ee48-8958-08fe834ea18d

--- models1/tblalarm.js
+++ models1/tblalarm.js
@@ -12,7 +12,7 @@ module.exports = function(sequelize, DataTypes) {
       type: DataTypes.STRING,
       allowNull: false
     },
-    Longtitude: {
+    Longitude: {
       type: DataTypes.STRING,
       allowNull: false
     },
--- models1/tblgpsdata.js
+++ models1/tblgpsdata.js
@@ -16,7 +16,7 @@ module.exports = function(sequelize, DataTypes) {
       type: DataTypes.STRING,
       allowNull: false
     },
-    Longtitude: {
+    Longitude: {
       type: DataTypes.STRING,
       allowNull: false
     },
