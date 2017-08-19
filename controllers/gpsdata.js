b2d752f5a0ad76f05b39ebd71fd46993785bea04
jaimin <jaimin@bugzstudio.com>
2017-08-19 04:45:58 +0000

    Hide not use column in Gps data report
    git-svn-id: https://svn.bugzstudio.com:7777/svn/GPScannerAPI@146 c13d9488-3209-ee48-8958-08fe834ea18d

--- controllers/gpsdata.js
+++ controllers/gpsdata.js
@@ -241,26 +241,27 @@ router.get('/ExportAllGpsData', function(req, res) {
         }, {
             caption: 'IsDoor',
             type: 'string'
-        }, {
-            caption: 'IsEngine',
-            type: 'string'
-        }, {
-            caption: 'CreatedDate',
-            type: 'string'
-        }, {
-            caption: 'Altitude',
-            type: 'string'
-        }, {
-            caption: 'AD1',
-            type: 'string'
         },
-        {
-            caption: 'AD2',
-            type: 'string'
-        }, {
-            caption: 'OdoMeter',
-            type: 'string'
-        }
+        /*{
+                   caption: 'IsEngine',
+                   type: 'string'
+               }, {
+                   caption: 'CreatedDate',
+                   type: 'string'
+               }, {
+                   caption: 'Altitude',
+                   type: 'string'
+               }, {
+                   caption: 'AD1',
+                   type: 'string'
+               },
+               {
+                   caption: 'AD2',
+                   type: 'string'
+               }, {
+                   caption: 'OdoMeter',
+                   type: 'string'
+               }*/
     ];
 
 
