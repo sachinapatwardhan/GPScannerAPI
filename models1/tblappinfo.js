b8129e68ca64ca3f20f1e55a468ecea34580fd61
jaimin <jaimin@bugzstudio.com>
2017-09-01 08:07:06 +0000

    crate new table tblappinfo
    git-svn-id: https://svn.bugzstudio.com:7777/svn/GPScannerAPI@228 c13d9488-3209-ee48-8958-08fe834ea18d

---
+++ models1/tblappinfo.js
@@ -0,0 +1,50 @@
+/* jshint indent: 2 */
+
+module.exports = function(sequelize, DataTypes) {
+  return sequelize.define('tblappinfo', {
+    Id: {
+      type: DataTypes.INTEGER(11),
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    AppName: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    BundleId: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    IOSCertificate: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    IOSKey: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    AndroidId: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    AndroidSenderId: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    CreatedDate: {
+      type: DataTypes.DATE,
+      allowNull: true
+    },
+    CreatedBy: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    tblappinfocol: {
+      type: DataTypes.STRING,
+      allowNull: true
+    }
+  }, {
+    tableName: 'tblappinfo'
+  });
+};
