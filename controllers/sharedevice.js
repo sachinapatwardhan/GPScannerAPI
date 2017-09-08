f631f28f32529032b71242f096732b07f5cfbe33
kosthi <kosthi@bugzstudio.com>
2017-09-08 09:31:21 +0000

    add api to manage using appId.
    git-svn-id: https://svn.bugzstudio.com:7777/svn/GPScannerAPI@255 c13d9488-3209-ee48-8958-08fe834ea18d

--- controllers/sharedevice.js
+++ controllers/sharedevice.js
@@ -3,13 +3,10 @@ var User = models.tbluserinformation;
 var SharedDevice = models.tblsharedevice;
 
 router.get('/GetAllSharedDeviceByUser', function(req, res) {
-    console.log("=============================================")
     SharedDevice.findAll({
         where: { DeviceId: req.query.DeviceId, $or: [{ idSharedUser: req.query.idSharedUser }, { idUser: req.query.idSharedUser }] },
         order: 'CreatedDate DESC'
     }).then(function(response) {
-        console.log("=============================================")
-
         res.json(response);
     }).catch(function(error) {
         res.json(error);
@@ -25,6 +22,58 @@ router.post('/SaveSharedUser', jsonParser, function(req, res) {
     if (token) {
         var decoded = jwt.decode(token, TokenKey);
         User.findOne({ where: { email: objUser.email } }).then(function(chkUserExist) {
+            if (chkUserExist != null) {
+                objUser.idUser = chkUserExist.id;
+                objUser.CreatedDate = new Date();
+                objUser.CreatedBy = decoded.username;
+                SharedDevice.findOne({ where: { DeviceId: objUser.DeviceId, idUser: objUser.idUser, idSharedUser: objUser.idSharedUser } }).then(function(resExist) {
+                    if (resExist != null) {
+                        res.json({
+                            success: false,
+                            message: "Vehicle is already Shared With This User...",
+                            data: resExist
+                        });
+
+                    } else {
+                        SharedDevice.create(objUser).then(function(response) {
+                            funAuditLog.CreateAuditLog('ShareDevice', decoded.username, 'Share Vehicle');
+                            res.json({
+                                success: true,
+                                message: "Vehicle Shared successfully...",
+                                data: response
+                            });
+
+                            var objConnection = {
+                                UserId: objUser.idUser,
+                                DeviceId: objUser.DeviceId
+                            }
+                            io.sockets.emit('ShareStatus', JSON.stringify(objConnection));
+                            io.sockets.emit(objUser.idUser + 'ShareStatus', JSON.stringify(objConnection));
+                        })
+                    }
+                })
+            } else {
+                res.json({
+                    success: false,
+                    message: "User is not Exist...",
+                    data: 1,
+                });
+            }
+        })
+    } else {
+        res.json(InvalidToken);
+    }
+})
+
+router.post('/SaveSharedUserNew', jsonParser, function(req, res) {
+    objUser = req.body;
+
+    objHeader = req.headers;
+
+    var token = getToken(objHeader);
+    if (token) {
+        var decoded = jwt.decode(token, TokenKey);
+        User.findOne({ where: { email: objUser.email, idApp: objUser.idApp } }).then(function(chkUserExist) {
             if (chkUserExist != null) {
                 objUser.idUser = chkUserExist.id;
                 objUser.CreatedDate = new Date();
