47a900e4d96939d828f8fe49e83286845a505087
jaimin <jaimin@bugzstudio.com>
2017-09-14 08:02:52 +0000

    add sim controller
    git-svn-id: https://svn.bugzstudio.com:7777/svn/GPScannerAPI@268 c13d9488-3209-ee48-8958-08fe834ea18d

--- server.js
+++ server.js
@@ -332,6 +332,7 @@ app.use('/homepage', require('./controllers/homepage'))
 app.use('/Report', require('./controllers/Report'));
 app.use('/appinfo', require('./controllers/appinfo'));
 app.use('/appversion', require('./controllers/appversion'));
+app.use('/sim', require('./controllers/sim'));
 
 //socket API End
 
