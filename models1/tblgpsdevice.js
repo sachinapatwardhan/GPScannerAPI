28e3dace2e70d76964e7620e23d5b0d434c7e97e
soham <soham@bugzstudio.com>
2017-10-02 04:34:55 +0000

    Add ActivationDate field
    git-svn-id: https://svn.bugzstudio.com:7777/svn/GPScannerAPI@327 c13d9488-3209-ee48-8958-08fe834ea18d

--- models1/tblgpsdevice.js
+++ models1/tblgpsdevice.js
@@ -1,83 +1,87 @@
 /* jshint indent: 2 */
 
 module.exports = function(sequelize, DataTypes) {
-    return sequelize.define('tblgpsdevice', {
-        id: {
-            type: DataTypes.INTEGER(11),
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true
-        },
-        DeviceId: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        IMEI: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        CreatedDate: {
-            type: DataTypes.DATE,
-            allowNull: true
-        },
-        Type: {
-            type: DataTypes.STRING,
-            allowNull: true
-        },
-        Version: {
-            type: DataTypes.STRING,
-            allowNull: true
-        },
-        CreatedBy: {
-            type: DataTypes.STRING,
-            allowNull: true
-        },
-        CountryId: {
-            type: DataTypes.INTEGER(11),
-            allowNull: true,
-            references: {
-                model: 'tblcountrymgmt',
-                key: 'id'
-            }
-        },
-        TelCoId: {
-            type: DataTypes.INTEGER(11),
-            allowNull: true,
-            references: {
-                model: 'tbltelco',
-                key: 'id'
-            }
-        },
-        SimNum: {
-            type: DataTypes.STRING,
-            allowNull: true
-        },
-        idSalesAgent: {
-            type: DataTypes.INTEGER(11),
-            allowNull: true,
-            references: {
-                model: 'tbluserinformation',
-                key: 'id'
-            }
-        },
-        IsActive: {
-            type: DataTypes.INTEGER(2),
-            allowNull: true,
-            defaultValue: '0'
-        },
-        ExpiryDate: {
-            type: DataTypes.DATE,
-            allowNull: true
-        },
-        AppName: {
-            type: DataTypes.STRING,
-            allowNull: true
-        },
-        idSim: {
-            type: DataTypes.INTEGER(11),
-            allowNull: true,
-        },
-    }, {
-        tableName: 'tblgpsdevice'
-    });
+  return sequelize.define('tblgpsdevice', {
+    id: {
+      type: DataTypes.INTEGER(11),
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    DeviceId: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    IMEI: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    CreatedDate: {
+      type: DataTypes.DATE,
+      allowNull: true
+    },
+    Type: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    Version: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    CreatedBy: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    CountryId: {
+      type: DataTypes.INTEGER(11),
+      allowNull: true,
+      references: {
+        model: 'tblcountrymgmt',
+        key: 'id'
+      }
+    },
+    TelCoId: {
+      type: DataTypes.INTEGER(11),
+      allowNull: true,
+      references: {
+        model: 'tbltelco',
+        key: 'id'
+      }
+    },
+    SimNum: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    idSalesAgent: {
+      type: DataTypes.INTEGER(11),
+      allowNull: true,
+      references: {
+        model: 'tbluserinformation',
+        key: 'id'
+      }
+    },
+    IsActive: {
+      type: DataTypes.INTEGER(2),
+      allowNull: true,
+      defaultValue: '0'
+    },
+    ExpiryDate: {
+      type: DataTypes.DATE,
+      allowNull: true
+    },
+    AppName: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    idSim: {
+      type: DataTypes.INTEGER(11),
+      allowNull: true
+    },
+    ActivationDate: {
+      type: DataTypes.DATE,
+      allowNull: true
+    }
+  }, {
+    tableName: 'tblgpsdevice'
+  });
 };
