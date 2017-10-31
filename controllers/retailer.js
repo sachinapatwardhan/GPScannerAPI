1f26624ca06672b79c850ce637e8495efb1a2192
soham <soham@bugzstudio.com>
2017-10-31 11:59:07 +0000

    Dino version 397 merge
    git-svn-id: https://svn.bugzstudio.com:7777/svn/GPScannerAPI@400 c13d9488-3209-ee48-8958-08fe834ea18d

--- controllers/retailer.js
+++ controllers/retailer.js
@@ -10,14 +10,164 @@
 	//////////
 
 	var DeviceAgentRetailer = models.tbldeviceagentretailer;
+	var Vehicle = models.tblvehicle;
+	var User = models.tbluserinformation;
 
 	//////////
+
+	router.get('/getPagedClientsByRetailerId', function(req, res) {
+		DeviceAgentRetailer.belongsTo(Vehicle, {
+			targetKey: 'deviceid',
+			foreignKey: {
+				name: 'deviceId',
+				allowNull: false
+			}
+		});
+
+		Vehicle.belongsTo(User, {
+			foreignKey: {
+				name: 'iduser',
+				allowNull: false
+			}
+		});
+
+		var include = {
+			model: User,
+			attributes: ['ProfileName', 'email', 'phone']
+		};
+		if (req.query.search.value) {
+			include.where = {
+				$or: [
+					{ ProfileName: { $like: '%' + req.query.search.value + '%' } },
+					{ email: { $like: '%' + req.query.search.value + '%' } },
+					{ phone: { $like: '%' + req.query.search.value + '%' } }
+				]
+			};
+		}
+
+		DeviceAgentRetailer.findAndCountAll({
+			where: {
+				retailerId: req.query.retailerId	
+			},
+			include: [{
+				model: Vehicle,
+				include: [include]
+			}]
+		})
+		.then(function(result) {
+			if (result.rows.length) {
+				res.json({
+					success: true,
+					message: 'Record(s) found.',
+					data: result.rows,
+					draw: req.query.draw,
+					recordsTotal: result.count,
+					recordsFiltered: result.count
+				});
+			} else {
+				res.json({
+					success: false,
+					message: 'No record(s) found.',
+					data: result.rows,
+					draw: req.query.draw,
+					recordsTotal: result.count,
+					recordsFiltered: result.count
+				});
+			}
+		})
+		.catch(function(err) {
+			res.json({
+				success: false,
+				message: 'Record(s) not found.'
+			});
+		});
+	});
+
+	router.get('/getAutocompleteActivateDeviceIds', function(req, res) {
+		var now = moment();
+
+		DeviceAgentRetailer.findAll({
+			where: {
+				deviceId: {
+					$like: '%' + req.query.deviceId + '%'
+				},
+				$or: [
+					{ activatedDatetime: { $eq: null } },
+					{ expiryDatetime: { $lt: now } }
+				]
+			},
+			limit: 20
+		})
+		.then(function(rGpsDevices) {
+			if (rGpsDevices.length) {
+				res.json({
+					success: true,
+					message: 'Record(s) found.',
+					data: rGpsDevices
+				});
+			} else {
+				res.json({
+					success: false,
+					message: 'No record(s) found.'
+				});
+			}
+		})
+		.catch(function(err) {
+			res.json({
+				success: false,
+				message: 'Record(s) not found.'
+			});
+		});
+	});
+
+	router.get('/getAutocompleteReconfigureDeviceIds', function(req, res) {
+		var now = moment();
+
+		DeviceAgentRetailer.findAll({
+			where: {
+				deviceId: {
+					$like: '%' + req.query.deviceId + '%'
+				},
+				activatedDatetime: {
+					$lt: now
+				},
+				expiryDatetime: {
+					$gt: now
+				}
+			},
+			limit: 20
+		})
+		.then(function(rGpsDevices) {
+			if (rGpsDevices.length) {
+				res.json({
+					success: true,
+					message: 'Record(s) found.',
+					data: rGpsDevices
+				});
+			} else {
+				res.json({
+					success: false,
+					message: 'No record(s) found.'
+				});
+			}
+		})
+		.catch(function(err) {
+			res.json({
+				success: false,
+				message: 'Record(s) not found.'
+			});
+		});
+	});
 
 	router.post('/activateDevice', jsonParser, function(req, res) {
 		DeviceAgentRetailer.findOne({
 			where: {
 				deviceId: req.body.deviceId
-			}
+			},
+			$or: [
+				{ activatedDatetime: { $eq: null } },
+				{ expiryDatetime: { $ne: null } }
+			]
 		})
 		.then(function(rDeviceAgentRetailer) {
 			if (!rDeviceAgentRetailer) {
@@ -25,13 +175,19 @@
 				err.name = 'BugzError';
 				throw err;
 			}
-
-			var date = new Date();
+			
 			return rDeviceAgentRetailer.update({
 				retailerId: req.body.retailerId,
 				activatedDatetime: moment(),
 				expiryDatetime: moment().add(1, 'year'),
 				lastModifiedDatetime: moment()
+			});
+		})
+		.then(function(rDeviceAgentRetailer) {
+			res.json({
+				success: true,
+				message: 'Device activated!',
+				data: rDeviceAgentRetailer
 			});
 		})
 		.catch(function(err) {
