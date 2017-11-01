5fb803cfb9449e936a31c2308d6dd359c5b9ad20
soham <soham@bugzstudio.com>
2017-11-01 06:01:11 +0000

    Total Count Notification api query issue solve
    git-svn-id: https://svn.bugzstudio.com:7777/svn/GPScannerAPI@413 c13d9488-3209-ee48-8958-08fe834ea18d

--- controllers/petAlarm.js
+++ controllers/petAlarm.js
@@ -267,7 +267,7 @@ router.post('/UpdateReadStatus', jsonParser, function(req, res) {
 })
 
 router.get('/GetTotalNotificationCount', function(req, res) {
-    var query = "select count(tp.Id) as TotalNotificationCount from tblalarm tp Where tp.IsRead=false and tp.DeviceId '" + req.query.DeviceId + "'";
+    var query = "select count(tp.Id) as TotalNotificationCount from tblalarm tp Where tp.IsRead=false and tp.DeviceId = '" + req.query.DeviceId + "'";
     connection.query(query, function(err, rows, fields) {
         if (!err) {
             res.json({ success: true, data: rows });
