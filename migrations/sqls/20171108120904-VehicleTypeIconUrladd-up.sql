fb40c7c33bc50ffcd569bc5b511a0f537084aed7
soham <soham@bugzstudio.com>
2017-11-08 12:13:25 +0000

    Vehicle Type 3 Icon URL add query add and db-migration
    git-svn-id: https://svn.bugzstudio.com:7777/svn/GPScannerAPI@440 c13d9488-3209-ee48-8958-08fe834ea18d

---
+++ migrations/sqls/20171108120904-VehicleTypeIconUrladd-up.sql
@@ -0,0 +1,10 @@
+/* Replace with your SQL commands */
+
+ALTER TABLE tblvehicletype
+Add OnIcon VARCHAR(100);
+
+ALTER TABLE tblvehicletype
+Add OffIcon VARCHAR(100);
+
+ALTER TABLE tblvehicletype
+Add ActiveIcon VARCHAR(100);
