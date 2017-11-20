9214c39900e6a266482d09b3797a5b8eac180ff2
dino <dino@bugzstudio.com>
2017-11-20 02:13:12 +0000

    20/11/2017 10:13am 1. Added sharedevice.spec.js.
    git-svn-id: https://svn.bugzstudio.com:7777/svn/GPScannerAPI@484 c13d9488-3209-ee48-8958-08fe834ea18d

---
+++ revision374/unitTests/sharedevice.spec.js
@@ -0,0 +1,319 @@
+var request = require('supertest');
+var require = require('really-need');
+var expect = require('chai').expect;
+var qs = require('qs');
+
+var testUser1 = {
+	email: 'unittest.user1@bugzstudio.com',
+	username: 'unittest.user1@bugzstudio.com',
+	password: 'unittest.user1',
+	ProfileName: 'UnitTestUser1',
+	IsMobileVerify: false,
+	idApp: 1
+};
+var testUser2 = {
+	email: 'unittest.user2@bugzstudio.com',
+	username: 'unittest.user2@bugzstudio.com',
+	password: 'unittest.user2',
+	ProfileName: 'UnitTestUser2',
+	IsMobileVerify: false,
+	idApp: 1
+};
+var testVehicle1 = {
+	id: 0,
+	iduser: 0,
+	Name: 'UnitTestVehicle1',
+	deviceid: '1234567890123x',
+	renewaldate: new Date(),
+	IsOnline: false,
+	HandshakDatetime: new Date(),
+	CreatedDate: new Date(),
+	MaxSpeed: 0.0000,
+	IsACC: false,
+	BatteryPercentage: 0,
+	CreatedBy: 'Admin',
+	ModifiedDate: null,
+	ModifiedBy: null,
+	SleepMode: false,
+	GPRSInterval: 10,
+	GPRSStopInterval: 0,
+	Arm: false,
+	OdoMeter: false,
+	HeartbeatInterval: true,
+	Relay: false,
+	Siren: false,
+	UserDefined: false,
+	DoorLock: false,
+	DoorUnlock: false,
+	TimeZone: null,
+	IsDelete: false,
+	DeviceType: 'UnitTest1',
+	idSalesAgent: null,
+	LastArmSetting: false,
+	IsShared: false,
+	InsurenceDate: new Date(),
+	PUCDate: null,
+	idType: null,
+	Movement: false
+};
+var testSharedDevice1 = {
+	id: 0,
+	idUser: 0,
+	idSharedUser: 0,
+	IsActive: true,
+	idVehicle: 0,
+	CreatedBy: 'UnitTest1',
+	CreatedDate: new Date(),
+	DeviceId: '1234567890123x',
+	IsSharedUserNotification: true,
+	IsNotification: true
+};
+var testSharedDevice2 = {
+	id: 0,
+	idUser: 0,
+	idSharedUser: 0,
+	IsActive: true,
+	idVehicle: 0,
+	CreatedBy: 'UnitTest1',
+	CreatedDate: new Date(),
+	DeviceId: '1234567890124x',
+	IsSharedUserNotification: true,
+	IsNotification: true
+};
+
+describe('/sharedevice', function() {
+	this.timeout(5000);
+	
+	var server;
+	var User;
+	var Vehicle;
+	var SharedDevice;
+	var dUser;
+	var dUser2;
+	var dVehicle;
+	var dSharedDevice;
+	var dSharedDevice2;
+
+	before(function(done) {
+		server = require('../server', {
+			bustCache: true
+		});
+		
+		User = models.tbluserinformation;
+		Vehicle = models.tblvehicle;
+		SharedDevice = models.tblsharedevice;
+
+		User.create(testUser1)
+		.then(function(rUser) {
+			dUser = rUser;
+			return User.create(testUser2);
+		})
+		.then(function(rUser) {
+			dUser2 = rUser;
+			testVehicle1.iduser = dUser.id;
+			return Vehicle.create(testVehicle1);
+		})
+		.then(function(rVehicle) {
+			dVehicle = rVehicle;
+			testSharedDevice1.idUser = dUser.id;
+			testSharedDevice1.idSharedUser = dUser2.id;
+			return SharedDevice.create(testSharedDevice1);
+		})
+		.then(function(rSharedDevice) {
+			dSharedDevice = rSharedDevice;
+			done();
+		});
+	});
+
+	after(function(done) {
+		SharedDevice.destroy({
+			where: {
+				CreatedBy: {
+					$like: 'UnitTest%'
+				}
+			}
+		})
+		.then(function() {
+			return dSharedDevice.destroy();
+		})
+		.then(function() {
+			return dVehicle.destroy();
+		})
+		.then(function() {
+			return dUser2.destroy();
+		})
+		.then(function() {
+			return dUser.destroy();
+		})
+		.then(function() {
+			server.close(done);
+		});
+	});
+
+	describe('/sharedevice/GetAllSharedDeviceByUser', function() {
+		it('should get all shared device by user', function(done) {
+			request(server)
+			.get('/sharedevice/GetAllSharedDeviceByUser')
+			.query(qs.stringify({
+				DeviceId: testSharedDevice1.DeviceId,
+				idSharedUser: dUser2.id
+			}))
+			.end(function(err, res) {
+				expect(res.body).to.exist;
+				expect(res.body).to.be.an('array').that.has.lengthOf(1);
+				expect(res.body[0]).to.be.an('object').that.has.property('CreatedBy').that.is.equal(testSharedDevice1.CreatedBy);
+				done();
+			});
+		});
+
+		it('should fail when no shared device found', function(done) {
+			request(server)
+			.get('/sharedevice/GetAllSharedDeviceByUser')
+			.end(function(err, res) {
+				expect(res.body).to.exist;
+				expect(res.body).to.be.an('array').that.has.lengthOf(0);
+				done();
+			});
+		});
+	});
+
+	describe('/sharedevice/SaveSharedUserNew', function() {
+		it('should save shared device if credentials are correct', function(done) {
+			var token = {
+				username: dUser.username,
+				password: dUser.password
+			};
+			token = 'JWT ' + jwt.encode(token, 'bugz');
+			testSharedDevice2.email = testUser1.email;
+			testSharedDevice2.idApp = testUser1.idApp;
+
+			request(server)
+			.post('/sharedevice/SaveSharedUserNew')
+			.set('authorization', token)
+			.send(testSharedDevice2)
+			.end(function(err, res) {
+				expect(res.body).to.exist;
+				expect(res.body.success).to.equal(true);
+				expect(res.body.message).to.equal('Vehicle Shared successfully...');
+				expect(res.body.data).to.be.an('object');
+				dSharedDevice2 = res.body.data;
+				done();
+			});
+		});
+
+		it('should fail when credentials are wrong', function(done) {
+			request(server)
+			.post('/sharedevice/SaveSharedUserNew')
+			.send(testSharedDevice2)
+			.end(function(err, res) {
+				expect(res.body).to.exist;
+				expect(res.body.success).to.equal(false);
+				expect(res.body.message).to.equal('Invalid token...');
+				expect(res.body.data).to.equal('TOKEN');
+				done();
+			});
+		});
+	});
+
+	describe('/sharedevice/ChangeSharedNotificationSetting', function() {
+		it('should change shared notification setting when credentials are correct', function(done) {
+			var token = {
+				username: dUser.username,
+				password: dUser.password
+			};
+			token = 'JWT ' + jwt.encode(token, 'bugz');
+			
+			request(server)
+			.get('/sharedevice/ChangeSharedNotificationSetting')
+			.set('authorization', token)
+			.query(qs.stringify(dSharedDevice2))
+			.end(function(err, res) {
+				expect(res.body).to.exist;
+				expect(res.body.success).to.equal(true);
+				expect(res.body.message).to.equal('Main Notification Setting Changed Successfully.');
+				done();
+			});
+		});
+		
+		it('should fail when credentials are wrong', function(done) {
+			request(server)
+			.get('/sharedevice/ChangeSharedNotificationSetting')
+			.query(qs.stringify(dSharedDevice2))
+			.end(function(err, res) {
+				expect(res.body).to.exist;
+				expect(res.body.success).to.equal(false);
+				expect(res.body.message).to.equal('Invalid token...');
+				expect(res.body.data).to.equal('TOKEN');
+				done();
+			});
+		});
+	});
+
+	describe('/sharedevice/ChangeNotificationSetting', function() {
+		it('should change notification setting when credentials are correct', function(done) {
+			var token = {
+				username: dUser.username,
+				password: dUser.password
+			};
+			token = 'JWT ' + jwt.encode(token, 'bugz');
+			
+			request(server)
+			.get('/sharedevice/ChangeNotificationSetting')
+			.set('authorization', token)
+			.query(qs.stringify(dSharedDevice2))
+			.end(function(err, res) {
+				expect(res.body).to.exist;
+				expect(res.body.success).to.equal(true);
+				expect(res.body.message).to.equal('Notification Setting Changed Successfully.');
+				done();
+			});
+		});
+		
+		it('should fail when credentials are wrong', function(done) {
+			request(server)
+			.get('/sharedevice/ChangeNotificationSetting')
+			.query(qs.stringify(dSharedDevice2))
+			.end(function(err, res) {
+				expect(res.body).to.exist;
+				expect(res.body.success).to.equal(false);
+				expect(res.body.message).to.equal('Invalid token...');
+				expect(res.body.data).to.equal('TOKEN');
+				done();
+			});
+		});
+	});
+
+	describe('/sharedevice/RemoveSharedUser', function() {
+		it('should remove shared user when credentials are correct', function(done) {
+			var token = {
+				username: dUser.username,
+				password: dUser.password
+			};
+			token = 'JWT ' + jwt.encode(token, 'bugz');
+			
+			request(server)
+			.get('/sharedevice/RemoveSharedUser')
+			.set('authorization', token)
+			.query(qs.stringify(dSharedDevice2))
+			.end(function(err, res) {
+				expect(res.body).to.exist;
+				expect(res.body.success).to.equal(true);
+				expect(res.body.message).to.equal('User Removed successfully...');
+				done();
+			});
+		});
+		
+		it('should fail when credentials are wrong', function(done) {
+			request(server)
+			.get('/sharedevice/RemoveSharedUser')
+			.query(qs.stringify(dSharedDevice2))
+			.end(function(err, res) {
+				expect(res.body).to.exist;
+				expect(res.body.success).to.equal(false);
+				expect(res.body.message).to.equal('Invalid token...');
+				expect(res.body.data).to.equal('TOKEN');
+				done();
+			});
+		});
+	});
+});
