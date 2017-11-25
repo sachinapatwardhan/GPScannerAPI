6291c5a3aeff34cd2c4e3916f92c1d7bacebc314
soham <soham@bugzstudio.com>
2017-11-25 09:25:56 +0000

    Add Script for Database changes - tblgpsdeletecash, tblsharedemail 2 table add - ios IMEI number table truncate (change to 15 digit imei) - various column add
    git-svn-id: https://svn.bugzstudio.com:7777/svn/GPScannerAPI@510 c13d9488-3209-ee48-8958-08fe834ea18d

---
+++ migrations/sqls/20171125043233-deletegpsandsharedeviceinvitation-up.sql
@@ -0,0 +1,53 @@
+/* Replace with your SQL commands */
+
+CREATE TABLE `tblgpsdeletecash` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `idVehicle` int(11) DEFAULT NULL,
+  `DeviceId` varchar(100) DEFAULT NULL,
+  `idUser` int(11) DEFAULT NULL,
+  `Status` varchar(45) DEFAULT NULL,
+  `CreatedDate` datetime DEFAULT NULL,
+  `CreatedBy` varchar(100) DEFAULT NULL,
+  `ModifiedDate` datetime DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE `tblsharedemail` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `DeviceId` varchar(100) DEFAULT NULL,
+  `idUser` int(11) DEFAULT NULL,
+  `SharedEmail` varchar(200) DEFAULT NULL,
+  `Status` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+ALTER TABLE tblpushnotification
+Add AppVersion VARCHAR(45) DEFAULT NULL;
+
+
+ALTER TABLE tbluserinformation
+Add Notification TINYINT(1) DEFAULT '0';
+
+
+ALTER TABLE tbluserinformation
+Add LastLogin DATETIME DEFAULT NULL;
+
+ALTER TABLE tblvehicletype
+Add LocateOnIcon varchar(100) DEFAULT NULL;
+
+ALTER TABLE tblvehicletype
+Add LocateOffIcon varchar(100) DEFAULT NULL;
+
+ALTER TABLE tblvehicletype
+Add LocateActiveIcon varchar(100) DEFAULT NULL;
+
+ALTER TABLE tblvehicletype
+Add LocateIsRotate TINYINT(1) DEFAULT '1';
+
+ALTER TABLE tbliosimeinumbermapping
+Add Type varchar(45) DEFAULT NULL;
+
+Truncate table tblimeinumber;
+Truncate table tbliosimeinumbermapping;
