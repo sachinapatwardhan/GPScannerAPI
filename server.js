50130e2e8c66c4fcbd0a1d6b06a386a4724aea66
jaimin <jaimin@bugzstudio.com>
2017-12-20 08:39:31 +0000

    Add pdf node module
    git-svn-id: https://svn.bugzstudio.com:7777/svn/GPScannerAPI@593 c13d9488-3209-ee48-8958-08fe834ea18d

--- server.js
+++ server.js
@@ -69,6 +69,7 @@ global.RoutePath = process.env.RoutePath;
 
 //Push Notification
 global.PushNotifications = require('node-pushnotifications');
+global.pdf = require('html-pdf');
 
 
 if (global.IsProduction == false || global.IsProduction == "false") {
