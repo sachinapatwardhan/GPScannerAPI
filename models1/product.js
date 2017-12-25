c7331dd7b5e3e8c33d5882cb52e0fc23f5904388
jay <jay@bugzstudio.com>
2017-12-25 06:20:27 +0000

    change table feild
    git-svn-id: https://svn.bugzstudio.com:7777/svn/GPScannerAPI@603 c13d9488-3209-ee48-8958-08fe834ea18d

--- models1/product.js
+++ models1/product.js
@@ -10,7 +10,11 @@ module.exports = function(sequelize, DataTypes) {
     },
     ProductTypeId: {
       type: DataTypes.INTEGER(11),
-      allowNull: false
+      allowNull: false,
+      references: {
+        model: 'tblappinfo',
+        key: 'Id'
+      }
     },
     ParentGroupedProductId: {
       type: DataTypes.INTEGER(11),
