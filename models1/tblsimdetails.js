8a98a3324800e02bf7fb04904880b3c1b3ad0a1a
jaimin <jaimin@bugzstudio.com>
2017-12-25 12:19:09 +0000

    Add appid column
    git-svn-id: https://svn.bugzstudio.com:7777/svn/GPScannerAPI@607 c13d9488-3209-ee48-8958-08fe834ea18d

--- models1/tblsimdetails.js
+++ models1/tblsimdetails.js
@@ -1,30 +1,34 @@
 /* jshint indent: 2 */
 
 module.exports = function(sequelize, DataTypes) {
-    return sequelize.define('tblsimdetails', {
-        id: {
-            type: DataTypes.INTEGER(11),
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true
-        },
-        SerialNum: {
-            type: DataTypes.STRING,
-            allowNull: true
-        },
-        PhoneNum: {
-            type: DataTypes.STRING,
-            allowNull: true
-        },
-        CreatedDate: {
-            type: DataTypes.DATE,
-            allowNull: true
-        },
-        idTelCo: {
-            type: DataTypes.INTEGER(11),
-            allowNull: true
-        }
-    }, {
-        tableName: 'tblsimdetails'
-    });
+  return sequelize.define('tblsimdetails', {
+    id: {
+      type: DataTypes.INTEGER(11),
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    SerialNum: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    PhoneNum: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    CreatedDate: {
+      type: DataTypes.DATE,
+      allowNull: true
+    },
+    idTelCo: {
+      type: DataTypes.INTEGER(11),
+      allowNull: true
+    },
+    idApp: {
+      type: DataTypes.INTEGER(11),
+      allowNull: true
+    }
+  }, {
+    tableName: 'tblsimdetails'
+  });
 };
