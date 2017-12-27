1e0025d5a27bac99eb459c3b7b47c92a82ec59b9
jaimin <jaimin@bugzstudio.com>
2017-12-27 10:17:24 +0000

    Add IdAdvanceFence new column
    git-svn-id: https://svn.bugzstudio.com:7777/svn/GPScannerAPI@621 c13d9488-3209-ee48-8958-08fe834ea18d

--- models1/tblfence.js
+++ models1/tblfence.js
@@ -1,52 +1,56 @@
 /* jshint indent: 2 */
 
 module.exports = function(sequelize, DataTypes) {
-    return sequelize.define('tblfence', {
-        id: {
-            type: DataTypes.INTEGER(11),
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true
-        },
-        name: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        deviceId: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        range: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        status: {
-            type: DataTypes.BOOLEAN,
-            allowNull: false
-        },
-        lat: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        lng: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        fencedraw: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        IsInFence: {
-            type: DataTypes.BOOLEAN,
-            allowNull: false,
-            defaultValue: '1'
-        },
-        IsFenceOnline: {
-            type: DataTypes.BOOLEAN,
-            allowNull: false,
-            defaultValue: '1'
-        }
-    }, {
-        tableName: 'tblfence'
-    });
+  return sequelize.define('tblfence', {
+    id: {
+      type: DataTypes.INTEGER(11),
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    deviceId: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    range: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    status: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false
+    },
+    lat: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    lng: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    fencedraw: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    IsInFence: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: '1'
+    },
+    IsFenceOnline: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: '1'
+    },
+    IdAdvanceFence: {
+      type: DataTypes.INTEGER(11),
+      allowNull: true
+    }
+  }, {
+    tableName: 'tblfence'
+  });
 };
