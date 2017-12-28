bc1aba8d00aa9b8a567a8c047e2d9a7550e1fb6c
jay <jay@bugzstudio.com>
2017-12-28 09:48:28 +0000

    git-svn-id: https://svn.bugzstudio.com:7777/svn/GPScannerAPI@635 c13d9488-3209-ee48-8958-08fe834ea18d

--- models1/tblwallettransaction.js
+++ models1/tblwallettransaction.js
@@ -10,7 +10,11 @@ module.exports = function(sequelize, DataTypes) {
     },
     idApp: {
       type: DataTypes.INTEGER(11),
-      allowNull: true
+      allowNull: true,
+      references: {
+        model: 'tblappinfo',
+        key: 'Id'
+      }
     },
     Amount: {
       type: DataTypes.DECIMAL,
