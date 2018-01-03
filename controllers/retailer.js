2e6d4c8c1cec3e95ad1cbcd19abfb546e87eeb62
jaimin <jaimin@bugzstudio.com>
2018-01-03 03:57:02 +0000

    call CreateDabitWalletTransactionGlobal  on device Activation time
    git-svn-id: https://svn.bugzstudio.com:7777/svn/GPScannerAPI@677 c13d9488-3209-ee48-8958-08fe834ea18d

--- controllers/retailer.js
+++ controllers/retailer.js
@@ -1,206 +1,244 @@
 (function() {
-	'use strict';
+    'use strict';
 
-	var express = require('express');
-	var router = express.Router();
-	var bodyParser = require('body-parser');
-	var jsonParser = bodyParser.json();
-	var moment = require('moment');
+    var express = require('express');
+    var router = express.Router();
+    var bodyParser = require('body-parser');
+    var jsonParser = bodyParser.json();
+    var moment = require('moment');
 
-	//////////
+    //////////
 
-	var DeviceAgentRetailer = models.tbldeviceagentretailer;
-	var Vehicle = models.tblvehicle;
-	var User = models.tbluserinformation;
+    var DeviceAgentRetailer = models.tbldeviceagentretailer;
+    var Vehicle = models.tblvehicle;
+    var User = models.tbluserinformation;
+    var SIM = models.tblsimdetails;
+    //////////
 
-	//////////
+    router.get('/getPagedClientsByRetailerId', function(req, res) {
+        DeviceAgentRetailer.belongsTo(Vehicle, {
+            targetKey: 'deviceid',
+            foreignKey: {
+                name: 'deviceId',
+                allowNull: false
+            }
+        });
 
-	router.get('/getPagedClientsByRetailerId', function(req, res) {
-		DeviceAgentRetailer.belongsTo(Vehicle, {
-			targetKey: 'deviceid',
-			foreignKey: {
-				name: 'deviceId',
-				allowNull: false
-			}
-		});
+        Vehicle.belongsTo(User, {
+            foreignKey: {
+                name: 'iduser',
+                allowNull: false
+            }
+        });
 
-		Vehicle.belongsTo(User, {
-			foreignKey: {
-				name: 'iduser',
-				allowNull: false
-			}
-		});
+        var include = {
+            model: User,
+            attributes: ['ProfileName', 'email', 'phone']
+        };
+        if (req.query.search.value) {
+            include.where = {
+                $or: [
+                    { ProfileName: { $like: '%' + req.query.search.value + '%' } },
+                    { email: { $like: '%' + req.query.search.value + '%' } },
+                    { phone: { $like: '%' + req.query.search.value + '%' } }
+                ]
+            };
+        }
 
-		var include = {
-			model: User,
-			attributes: ['ProfileName', 'email', 'phone']
-		};
-		if (req.query.search.value) {
-			include.where = {
-				$or: [
-					{ ProfileName: { $like: '%' + req.query.search.value + '%' } },
-					{ email: { $like: '%' + req.query.search.value + '%' } },
-					{ phone: { $like: '%' + req.query.search.value + '%' } }
-				]
-			};
-		}
+        DeviceAgentRetailer.findAndCountAll({
+                where: {
+                    retailerId: req.query.retailerId
+                },
+                include: [{
+                    model: Vehicle,
+                    include: [include]
+                }]
+            })
+            .then(function(result) {
+                if (result.rows.length) {
+                    res.json({
+                        success: true,
+                        message: 'Record(s) found.',
+                        data: result.rows,
+                        draw: req.query.draw,
+                        recordsTotal: result.count,
+                        recordsFiltered: result.count
+                    });
+                } else {
+                    res.json({
+                        success: false,
+                        message: 'No record(s) found.',
+                        data: result.rows,
+                        draw: req.query.draw,
+                        recordsTotal: result.count,
+                        recordsFiltered: result.count
+                    });
+                }
+            })
+            .catch(function(err) {
+                res.json({
+                    success: false,
+                    message: 'Record(s) not found.'
+                });
+            });
+    });
 
-		DeviceAgentRetailer.findAndCountAll({
-			where: {
-				retailerId: req.query.retailerId	
-			},
-			include: [{
-				model: Vehicle,
-				include: [include]
-			}]
-		})
-		.then(function(result) {
-			if (result.rows.length) {
-				res.json({
-					success: true,
-					message: 'Record(s) found.',
-					data: result.rows,
-					draw: req.query.draw,
-					recordsTotal: result.count,
-					recordsFiltered: result.count
-				});
-			} else {
-				res.json({
-					success: false,
-					message: 'No record(s) found.',
-					data: result.rows,
-					draw: req.query.draw,
-					recordsTotal: result.count,
-					recordsFiltered: result.count
-				});
-			}
-		})
-		.catch(function(err) {
-			res.json({
-				success: false,
-				message: 'Record(s) not found.'
-			});
-		});
-	});
+    router.get('/getAutocompleteActivateDeviceIds', function(req, res) {
+        var now = moment();
 
-	router.get('/getAutocompleteActivateDeviceIds', function(req, res) {
-		var now = moment();
+        DeviceAgentRetailer.findAll({
+                where: {
+                    deviceId: {
+                        $like: '%' + req.query.deviceId + '%'
+                    },
+                    $or: [
+                        { activatedDatetime: { $eq: null } },
+                        { expiryDatetime: { $lt: now } }
+                    ]
+                },
+                limit: 20
+            })
+            .then(function(rGpsDevices) {
+                if (rGpsDevices.length) {
+                    res.json({
+                        success: true,
+                        message: 'Record(s) found.',
+                        data: rGpsDevices
+                    });
+                } else {
+                    res.json({
+                        success: false,
+                        message: 'No record(s) found.'
+                    });
+                }
+            })
+            .catch(function(err) {
+                res.json({
+                    success: false,
+                    message: 'Record(s) not found.'
+                });
+            });
+    });
 
-		DeviceAgentRetailer.findAll({
-			where: {
-				deviceId: {
-					$like: '%' + req.query.deviceId + '%'
-				},
-				$or: [
-					{ activatedDatetime: { $eq: null } },
-					{ expiryDatetime: { $lt: now } }
-				]
-			},
-			limit: 20
-		})
-		.then(function(rGpsDevices) {
-			if (rGpsDevices.length) {
-				res.json({
-					success: true,
-					message: 'Record(s) found.',
-					data: rGpsDevices
-				});
-			} else {
-				res.json({
-					success: false,
-					message: 'No record(s) found.'
-				});
-			}
-		})
-		.catch(function(err) {
-			res.json({
-				success: false,
-				message: 'Record(s) not found.'
-			});
-		});
-	});
+    router.get('/getAutocompleteReconfigureDeviceIds', function(req, res) {
+        var now = moment();
 
-	router.get('/getAutocompleteReconfigureDeviceIds', function(req, res) {
-		var now = moment();
+        DeviceAgentRetailer.findAll({
+                where: {
+                    deviceId: {
+                        $like: '%' + req.query.deviceId + '%'
+                    },
+                    activatedDatetime: {
+                        $lt: now
+                    },
+                    expiryDatetime: {
+                        $gt: now
+                    }
+                },
+                limit: 20
+            })
+            .then(function(rGpsDevices) {
+                if (rGpsDevices.length) {
+                    res.json({
+                        success: true,
+                        message: 'Record(s) found.',
+                        data: rGpsDevices
+                    });
+                } else {
+                    res.json({
+                        success: false,
+                        message: 'No record(s) found.'
+                    });
+                }
+            })
+            .catch(function(err) {
+                res.json({
+                    success: false,
+                    message: 'Record(s) not found.'
+                });
+            });
+    });
 
-		DeviceAgentRetailer.findAll({
-			where: {
-				deviceId: {
-					$like: '%' + req.query.deviceId + '%'
-				},
-				activatedDatetime: {
-					$lt: now
-				},
-				expiryDatetime: {
-					$gt: now
-				}
-			},
-			limit: 20
-		})
-		.then(function(rGpsDevices) {
-			if (rGpsDevices.length) {
-				res.json({
-					success: true,
-					message: 'Record(s) found.',
-					data: rGpsDevices
-				});
-			} else {
-				res.json({
-					success: false,
-					message: 'No record(s) found.'
-				});
-			}
-		})
-		.catch(function(err) {
-			res.json({
-				success: false,
-				message: 'Record(s) not found.'
-			});
-		});
-	});
+    router.post('/activateDevice', jsonParser, function(req, res) {
+        console.log(req.body)
+        DeviceAgentRetailer.findOne({
+                where: {
+                    $or: {
+                        deviceId: req.body.deviceId,
+                        simSerial: req.body.simSerial
+                    }
+                },
+                $or: [
+                    { activatedDatetime: { $eq: null } },
+                    { expiryDatetime: { $ne: null } }
+                ]
+            })
+            .then(function(rDeviceAgentRetailer) {
+                // console.log("************************", rDeviceAgentRetailer)
+                if (!rDeviceAgentRetailer) {
+                    var err = new Error('GPS device not found.');
+                    err.name = 'BugzError';
+                    throw err;
+                } else {
+                    SIM.findOne({
+                            where: { SerialNum: req.body.simSerial }
+                        }).then(function(SIMExist) {
+                            if (!SIMExist) {
+                                var err = new Error('Sim Serial not found.');
+                                err.name = 'BugzError';
+                                throw err;
+                            }
+                            return rDeviceAgentRetailer.update({
+                                retailerId: req.body.retailerId,
+                                simSerial: req.body.simSerial,
+                                activatedDatetime: moment(),
+                                expiryDatetime: moment().add(1, 'year'),
+                                lastModifiedDatetime: moment()
+                            });
+                        }).then(function(rDeviceAgentRetailer) {
+                            User.findOne({ where: rDeviceAgentRetailer.agentId }).then(function(UserExist) {
 
-	router.post('/activateDevice', jsonParser, function(req, res) {
-		DeviceAgentRetailer.findOne({
-			where: {
-				$or: {
-					deviceId: req.body.deviceId,
-					simSerial: req.body.simSerial
-				}
-			},
-			$or: [
-				{ activatedDatetime: { $eq: null } },
-				{ expiryDatetime: { $ne: null } }
-			]
-		})
-		.then(function(rDeviceAgentRetailer) {
-			if (!rDeviceAgentRetailer) {
-				var err = new Error('GPS device not found.');
-				err.name = 'BugzError';
-				throw err;
-			}
-			
-			return rDeviceAgentRetailer.update({
-				retailerId: req.body.retailerId,
-				simSerial: req.body.simSerial,
-				activatedDatetime: moment(),
-				expiryDatetime: moment().add(1, 'year'),
-				lastModifiedDatetime: moment()
-			});
-		})
-		.then(function(rDeviceAgentRetailer) {
-			res.json({
-				success: true,
-				message: 'Device activated!',
-				data: rDeviceAgentRetailer
-			});
-		})
-		.catch(function(err) {
-			res.json({
-				success: false,
-				message: err.message
-			});
-		});
-	});
+                                // console.log(UserExist.country, "===", rDeviceAgentRetailer.deviceId, "===", UserExist.username, "===", UserExist.idApp)
+                                CreateDabitWalletTransactionGlobal(UserExist.country, rDeviceAgentRetailer.deviceId, UserExist.username, UserExist.idApp, function(resFlg) {
+                                    res.json({
+                                        success: true,
+                                        message: 'Device activated!',
+                                        data: rDeviceAgentRetailer
+                                    });
+                                })
+                            })
+                        })
+                        .catch(function(err) {
+                            console.log("err..", err);
+                            res.json({
+                                success: false,
+                                message: err.message
+                            });
+                        });
+                }
+            })
+            // .then(function(rDeviceAgentRetailer) {
+            //     User.findOne({ where: rDeviceAgentRetailer.agentId }).then(function(UserExist) {
+            //         console.log(rDeviceAgentRetailer)
 
-	module.exports = router;
+        //         console.log(UserExist.country, "===", rDeviceAgentRetailer.DeviceId, "===", UserExist.username, "===", UserExist.idApp)
+        //         CreateDabitWalletTransactionGlobal(UserExist.country, rDeviceAgentRetailer.DeviceId, UserExist.username, UserExist.idApp, function(resFlg) {
+        //             res.json({
+        //                 success: true,
+        //                 message: 'Device activated!',
+        //                 data: rDeviceAgentRetailer
+        //             });
+        //         })
+        //     })
+        // })
+        .catch(function(err) {
+            console.log("err..", err);
+            res.json({
+                success: false,
+                message: err.message
+            });
+        });
+    });
+
+    module.exports = router;
 })();
