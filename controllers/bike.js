22136e19e89a6078f69c6b00c13003684d10bfeb
jaimin <jaimin@bugzstudio.com>
2018-01-12 08:01:06 +0000

    get IsShared filed in GetAllWorkingBikeWebAppNew
    git-svn-id: https://svn.bugzstudio.com:7777/svn/GPScannerAPI@734 c13d9488-3209-ee48-8958-08fe834ea18d

--- controllers/bike.js
+++ controllers/bike.js
@@ -1699,11 +1699,11 @@ router.get('/GetAllWorkingBikeWebAppNew1', jsonParser, function(req, res) {
 
 
 router.get('/GetAllWorkingBikeWebAppNew', function(req, res) {
-    var query = "select t4.id,t4.iduser,t4.Name,t4.deviceid,t4.IsOnline,t4.DeviceType,t4.ShareId,t4.VehicleType, " +
+    var query = "select t4.id,t4.iduser,t4.Name,t4.deviceid,t4.IsOnline,t4.DeviceType,t4.ShareId,t4.VehicleType,t4.IsShared, " +
         "(select count(*) from tblalarm  a where a.DeviceId =t4.deviceid and IsRead=false) as 'NotificationCount' ," +
         "(SELECT COUNT(*) FROM tblserviceenhancementnotification WHERE IsRead=false and idvehicle = t4.id) as 'AlertCount' " +
         "from " +
-        "  (select tb.iduser,tb.deviceid,tb.id,tb.Name,tb.DeviceType,tvt.Type as 'VehicleType',tsd.id as'ShareId',tb.IsOnline from tblvehicle tb " +
+        "  (select tb.iduser,tb.deviceid,tb.id,tb.Name,tb.DeviceType,tb.IsShared as 'IsShared',tvt.Type as 'VehicleType',tsd.id as'ShareId',tb.IsOnline from tblvehicle tb " +
         "   LEFT join tblsharedevice tsd on tb.id=tsd.idVehicle " +
         "   LEFT JOIN tblvehicletype tvt on tb.idType=tvt.id " +
         "   where (tb.iduser=" + req.query.idUser + " or tsd.iduser=" + req.query.idUser + ") and IsDelete = false " +
@@ -1727,7 +1727,7 @@ router.get('/GetAllWorkingBikeWebAppNew', function(req, res) {
                     obj.VehicleType = rows[i].VehicleType;
                     obj.NotificationCount = rows[i].NotificationCount;
                     obj.AlertCount = rows[i].AlertCount;
-
+                    obj.IsShared = rows[i].IsShared;
                     client.get(rows[i].deviceid, function(err, strgpsdata) {
                         if (!err) {
                             if (strgpsdata != null & strgpsdata != '' && strgpsdata != undefined) {
