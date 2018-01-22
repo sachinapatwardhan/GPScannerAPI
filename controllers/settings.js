41662749d8f5fdd0a9bc92f522b120455f927fda
jaimin <jaimin@bugzstudio.com>
2018-01-22 07:11:23 +0000

    set audit log entry
    git-svn-id: https://svn.bugzstudio.com:7777/svn/GPScannerAPI@755 c13d9488-3209-ee48-8958-08fe834ea18d

--- controllers/settings.js
+++ controllers/settings.js
@@ -115,32 +115,56 @@ router.get('/GetTaxSettingByName', function(req, res) {
     })
 })
 
-router.get('/GetTaxSettingForSharlink', function(req, res) {
-    TaxSetting.findOne({ where: { Name: req.query.TaxSettingName } }).then(function(response) {
-        if (response != null) {
-            Vehicle.findOne({ where: { deviceid: req.query.DeviceId } }).then(function(VehicleExist) {
-                if (VehicleExist.ShareCode != null && VehicleExist.ShareCode != '' && VehicleExist.ShareCode != undefined) {
-                    var response1 = new Object();
-                    response1.Name = response.Name
-                    var url = req.query.DeviceId + "," + VehicleExist.ShareCode;
-                    response1.enDeviceId = jwt.encode(url, "bugz");
-                    response1.link = response.Value + '/' + response1.enDeviceId;
-                    res.json({ success: true, message: "Record found...", data: response1 });
-                } else {
-                    VehicleExist.updateAttributes({ ShareCode: Math.floor(100000 + Math.random() * 900000) }).then(function(shareCodeupdated) {
-                        var response1 = new Object();
-                        response1.Name = response.Name
-                        var url = req.query.DeviceId + "," + shareCodeupdated.ShareCode;
-                        response1.enDeviceId = jwt.encode(url, "bugz");
-                        response1.link = response.Value + '/' + response1.enDeviceId;
-                        res.json({ success: true, message: "Record found...", data: response1 });
-                    })
-                }
-            })
-        } else {
-            res.json({ success: false, message: "Record not found...", data: response });
-        }
-    })
+router.get('/GetTaxSettingForSharlink', jsonParser, function(req, res) {
+    objHeader = req.headers;
+    var token = getToken(objHeader);
+    if (token) {
+        var decoded = jwt.decode(token, TokenKey);
+        User.findOne({
+            where: {
+                username: decoded.username,
+                password: decoded.password
+            }
+        }).then(function(UserExist) {
+            if (UserExist != null) {
+                TaxSetting.findOne({ where: { Name: req.query.TaxSettingName } }).then(function(response) {
+                    if (response != null) {
+                        Vehicle.findOne({ where: { deviceid: req.query.DeviceId } }).then(function(VehicleExist) {
+                            if (VehicleExist.ShareCode != null && VehicleExist.ShareCode != '' && VehicleExist.ShareCode != undefined) {
+                                var response1 = new Object();
+                                response1.Name = response.Name
+                                var url = req.query.DeviceId + "," + VehicleExist.ShareCode;
+                                response1.enDeviceId = jwt.encode(url, "bugz");
+                                response1.link = response.Value + '/' + response1.enDeviceId;
+                                res.json({ success: true, message: "Record found...", data: response1 });
+                            } else {
+                                VehicleExist.updateAttributes({ ShareCode: Math.floor(100000 + Math.random() * 900000) }).then(function(shareCodeupdated) {
+                                    funAuditLog.CreateAuditLog('Insert share Location code', UserExist.username, 'Insert share Location code (' + req.query.DeviceId + ')');
+                                    var response1 = new Object();
+                                    response1.Name = response.Name
+                                    var url = req.query.DeviceId + "," + shareCodeupdated.ShareCode;
+                                    response1.enDeviceId = jwt.encode(url, "bugz");
+                                    response1.link = response.Value + '/' + response1.enDeviceId;
+                                    res.json({ success: true, message: "Record found...", data: response1 });
+                                })
+                            }
+                        })
+                    } else {
+                        res.json({ success: false, message: "Record not found...", data: response });
+                    }
+                })
+            } else {
+                res.json(InvalidToken);
+            }
+        })
+    } else {
+        res.json(InvalidToken);
+    }
+
+
+
+
+
 })
 
 router.get('/GetAllSettingByNamelist', function(req, res) {
