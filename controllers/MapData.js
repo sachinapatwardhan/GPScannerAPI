8f282336ae7682d910c908e805c4353ae93b1289
jaimin <jaimin@bugzstudio.com>
2018-03-01 05:14:56 +0000

    replace IsEngine to IsPatchEngine
    git-svn-id: https://svn.bugzstudio.com:7777/svn/GPScannerAPI@815 c13d9488-3209-ee48-8958-08fe834ea18d

--- controllers/MapData.js
+++ controllers/MapData.js
@@ -64,7 +64,7 @@ router.get('/ExportReport', function(req, res) {
     var convertDate = convertdateformatForUnix(Enddate);
     var unixEnddate = new Date(convertDate.replace(' ', 'T')).getTime() / 1000;
 
-    var query = "select Id,Datetime, Latitude, Longitude, GPSPositioning, Speed, Direction, DeviceId,IsEngine, Date from tblgpsdata where deviceid=" + req.query.DeviceId + " and Date >= '" + unixStartdate + "' and Date <= '" + unixEnddate + "' order by Date;"
+    var query = "select Id,Datetime, Latitude, Longitude, GPSPositioning, Speed, Direction, DeviceId,IsPatchEngine as IsEngine, Date from tblgpsdata where deviceid=" + req.query.DeviceId + " and Date >= '" + unixStartdate + "' and Date <= '" + unixEnddate + "' order by Date;"
 
     connection.query(query, function(err, response) {
         conf.rows = [];
@@ -125,6 +125,165 @@ router.get('/ExportReport', function(req, res) {
                 row.push(Datetime.toString(), latlng, Speed.toString(), GPSPositioning.toString(), Direction.toString(), IsEngine.toString());
                 conf.rows.push(row);
                 GetData(i + 1);
+            } else {
+                var result = nodeExcel.execute(conf);
+                res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+                res.setHeader("Content-Disposition", "attachment; filename=TrackDetail.xlsx");
+                res.end(result, 'binary');
+            }
+        }
+    })
+})
+
+
+router.get('/ExportReportNew', function(req, res) {
+    objTask = req.query;
+    var conf = {};
+    conf.name = "sheet1";
+    conf.cols = [{
+            caption: 'Date Time',
+            type: 'string'
+        },
+        {
+            caption: 'Address',
+            type: 'string'
+        },
+        // {
+        //     caption: 'Latitude/Longitude',
+        //     type: 'string'
+        // },
+        {
+            caption: 'Speed (km/h)',
+            type: 'string'
+        }, {
+            caption: 'GPS Positioning',
+            type: 'string'
+        }, {
+            caption: 'Direction',
+            type: 'string'
+        }, {
+            caption: 'Is Engine',
+            type: 'string'
+        }
+    ];
+    var Startdate = objTask.StartDate;
+    var Enddate = objTask.EndDate;
+
+    var convertDate = convertdateformatForUnix(Startdate);
+    var unixStartdate = new Date(convertDate.replace(' ', 'T')).getTime() / 1000;
+
+    var convertDate = convertdateformatForUnix(Enddate);
+    var unixEnddate = new Date(convertDate.replace(' ', 'T')).getTime() / 1000;
+
+    var query = "select Id,Datetime, Latitude, Longitude, GPSPositioning, Speed, Direction, DeviceId,IsPatchEngine as IsEngine, Date from tblgpsdata where deviceid=" + req.query.DeviceId + " and Date >= '" + unixStartdate + "' and Date <= '" + unixEnddate + "' order by Date;"
+    console.log(query)
+    connection.query(query, function(err, response) {
+        conf.rows = [];
+        var lstTemp = [];
+        var COuntEngineOff = 0;
+        for (var i = 0; i < response.length; i++) {
+            if (response[i].IsEngine == true) {
+                COuntEngineOff = 0;
+                lstTemp.push(response[i]);
+            } else {
+                if (COuntEngineOff == 0) {
+                    lstTemp.push(response[i]);
+                }
+                COuntEngineOff = COuntEngineOff + 1;
+            }
+        }
+        response = lstTemp;
+        GetData(0);
+
+        function GetData(i) {
+            if (i < response.length) {
+                var row = [];
+                var Datetime = 'N/A';
+                var longitude = 0.00;
+                var Longitude = 0.00;
+                var Speed = 0.00;
+                var GPSPositioning = 'N/A';
+                var Direction = 0.00;
+                var IsEngine = 'Off';
+                if (response[i].Latitude != undefined && response[i].Latitude != null && response[i].Latitude != '' && response[i].Longitude != undefined && response[i].Longitude != null && response[i].Longitude != '') {
+                    geocoder.reverse({ lat: response[i].Latitude, lon: response[i].Longitude }, function(err, resAddress) {
+                        if (response[i].Datetime != null && response[i].Datetime != '' && response[i].Datetime != undefined) {
+                            // Datetime = dateformat(response[i].Datetime, 2);
+                            Datetime = momentz.utc(new Date(response[i].Date * 1000)).tz(objTask.TimeZone).format('DD-MM-YYYY hh:mm:ss a')
+                        }
+
+                        if (response[i].Latitude != null && response[i].Latitude != '' && response[i].Latitude != undefined) {
+                            Latitude = response[i].Latitude;
+                        }
+                        if (response[i].Longitude != null && response[i].Longitude != '' && response[i].Longitude != undefined) {
+                            Longitude = response[i].Longitude;
+                        }
+                        if (response[i].GPSPositioning != null && response[i].GPSPositioning != '' && response[i].GPSPositioning != undefined) {
+                            GPSPositioning = response[i].GPSPositioning;
+                        }
+                        if (response[i].Speed != null && response[i].Speed != '' && response[i].Speed != undefined) {
+                            Speed = parseFloat(response[i].Speed).toFixed(2);
+                        }
+                        if (response[i].Direction != null && response[i].Direction != '' && response[i].Direction != undefined) {
+                            Direction = response[i].Direction;
+                        }
+                        if (response[i].IsEngine != null && response[i].IsEngine != '' && response[i].IsEngine != undefined) {
+                            if (response[i].IsEngine == 0 || response[i].IsEngine == false) {
+                                IsEngine = 'Off';
+                            } else {
+                                IsEngine = 'On';
+                            }
+                        }
+                        if (err == null && resAddress != null) {
+                            if (resAddress.length > 0) {
+                                Address = resAddress[0].formattedAddress;
+                                row.push(Datetime.toString(), Address, Speed.toString(), GPSPositioning.toString(), Direction.toString(), IsEngine.toString());
+                            } else {
+                                Address = "N/A";
+                                row.push(Datetime.toString(), Address, Speed.toString(), GPSPositioning.toString(), Direction.toString(), IsEngine.toString());
+                            }
+                        } else {
+                            Address = "N/A";
+                            row.push(Datetime.toString(), Address, Speed.toString(), GPSPositioning.toString(), Direction.toString(), IsEngine.toString());
+                        }
+                        var latlng = Latitude + "/" + Longitude;
+
+                        conf.rows.push(row);
+                        GetData(i + 1);
+                    })
+                } else {
+                    if (response[i].Datetime != null && response[i].Datetime != '' && response[i].Datetime != undefined) {
+                        // Datetime = dateformat(response[i].Datetime, 2);
+                        Datetime = momentz.utc(new Date(response[i].Date * 1000)).tz(objTask.TimeZone).format('DD-MM-YYYY hh:mm:ss a')
+                    }
+
+                    if (response[i].Latitude != null && response[i].Latitude != '' && response[i].Latitude != undefined) {
+                        Latitude = response[i].Latitude;
+                    }
+                    if (response[i].Longitude != null && response[i].Longitude != '' && response[i].Longitude != undefined) {
+                        Longitude = response[i].Longitude;
+                    }
+                    if (response[i].GPSPositioning != null && response[i].GPSPositioning != '' && response[i].GPSPositioning != undefined) {
+                        GPSPositioning = response[i].GPSPositioning;
+                    }
+                    if (response[i].Speed != null && response[i].Speed != '' && response[i].Speed != undefined) {
+                        Speed = parseFloat(response[i].Speed).toFixed(2);
+                    }
+                    if (response[i].Direction != null && response[i].Direction != '' && response[i].Direction != undefined) {
+                        Direction = response[i].Direction;
+                    }
+                    if (response[i].IsEngine != null && response[i].IsEngine != '' && response[i].IsEngine != undefined) {
+                        if (response[i].IsEngine == 0 || response[i].IsEngine == false) {
+                            IsEngine = 'Off';
+                        } else {
+                            IsEngine = 'On';
+                        }
+                    }
+                    var latlng = Latitude + "/" + Longitude;
+                    row.push(Datetime.toString(), latlng, Speed.toString(), GPSPositioning.toString(), Direction.toString(), IsEngine.toString());
+                    conf.rows.push(row);
+                    GetData(i + 1);
+                }
             } else {
                 var result = nodeExcel.execute(conf);
                 res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
