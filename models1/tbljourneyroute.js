15d0bb706b8b3c5ffc1afcd49c05bc671a09c703
jaimin <jaimin@bugzstudio.com>
2018-03-03 07:15:17 +0000

    git-svn-id: https://svn.bugzstudio.com:7777/svn/GPScannerAPI@821 c13d9488-3209-ee48-8958-08fe834ea18d

--- models1/tbljourneyroute.js
+++ models1/tbljourneyroute.js
@@ -5,8 +5,7 @@ module.exports = function(sequelize, DataTypes) {
     Id: {
       type: DataTypes.INTEGER(11),
       allowNull: false,
-      primaryKey: true,
-      autoIncrement: true
+      primaryKey: true
     },
     DeviceId: {
       type: DataTypes.STRING,
