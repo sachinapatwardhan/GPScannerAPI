0700f3ee3fa7743eb40cc8ba8c3a45581fd26ec0
jaimin <jaimin@bugzstudio.com>
2018-03-03 09:42:57 +0000

    change GetAllSharedUser
    git-svn-id: https://svn.bugzstudio.com:7777/svn/GPScannerAPI@823 c13d9488-3209-ee48-8958-08fe834ea18d

--- controllers/sharedevice.js
+++ controllers/sharedevice.js
@@ -41,7 +41,7 @@ router.get('/GetAllSharedUser', function(req, res) {
 
     if (objSearch != '' && objSearch != null && objSearch != undefined) {
         search = 'Where (tu.email like "%' + objSearch + '%" or ';
-        search = search + 'tv.sharedUser like "%' + objSearch + '%" or ';
+        search = search + 'tus.email like "%' + objSearch + '%" or ';
         search = search + 'tv.Name like "%' + objSearch + '%" or ';
         search = search + 'ts.DeviceId like "%' + objSearch + '%" or ';
         search = search + 'tai.AppName like "%' + objSearch + '%" or ';
@@ -55,17 +55,30 @@ router.get('/GetAllSharedUser', function(req, res) {
             search += ' Where tu.idApp =' + objParam.appId;
         }
     }
-    var query = " SELECT tai.AppName,ts.id,ts.idUser,ts.idSharedUser,tu.email,tv.sharedUser,tv.Name,ts.DeviceId,CONVERT_TZ(ts.CreatedDate,'+00:00','" + CurrentOffset + "') as CreatedDate" +
+
+    var query = " SELECT tai.AppName,ts.id,ts.idUser,ts.idSharedUser,tu.email,tus.email as sharedUser,tv.Name,ts.DeviceId,CONVERT_TZ(ts.CreatedDate,'+00:00','" + CurrentOffset + "') as CreatedDate" +
         " FROM tblsharedevice ts  LEFT JOIN tbluserinformation tu on tu.id = ts.idUser" +
         " LEFT Join tblappinfo tai ON tu.idApp = tai.id" +
-        " LEFT JOIN (SELECT tbluserinformation.email as 'sharedUser',tblvehicle.Name, tblvehicle.deviceid,tblvehicle.id,tblvehicle.iduser" +
-        " FROM tblvehicle LEFT JOIN tbluserinformation ON tbluserinformation.id= tblvehicle.iduser) as tv on tv.deviceid= ts.DeviceId " + search +
+        " LEFT JOIN tbluserinformation tus ON ts.idSharedUser = tus.id " +
+        " LEFT JOIN tblvehicle tv ON tv.deviceid = ts.DeviceId " + search +
         " order by " + Orderby + " limit " + parseInt(objParam.length) + " offset " + parseInt(objParam.start);
+    // var query = " SELECT tai.AppName,ts.id,ts.idUser,ts.idSharedUser,tu.email,tv.sharedUser,tv.Name,ts.DeviceId,CONVERT_TZ(ts.CreatedDate,'+00:00','" + CurrentOffset + "') as CreatedDate" +
+    //     " FROM tblsharedevice ts  LEFT JOIN tbluserinformation tu on tu.id = ts.idUser" +
+    //     " LEFT Join tblappinfo tai ON tu.idApp = tai.id" +
+    //     " LEFT JOIN (SELECT tbluserinformation.email as 'sharedUser',tblvehicle.Name, tblvehicle.deviceid,tblvehicle.id,tblvehicle.iduser" +
+    //     " FROM tblvehicle LEFT JOIN tbluserinformation ON tbluserinformation.id= tblvehicle.iduser) as tv on tv.deviceid= ts.DeviceId " + search +
+    //     " order by " + Orderby + " limit " + parseInt(objParam.length) + " offset " + parseInt(objParam.start);
+
     var Countqry = "SELECT count(ts.id) as TotalRecord " +
         " FROM tblsharedevice ts  LEFT JOIN tbluserinformation tu on tu.id = ts.idUser" +
         " LEFT Join tblappinfo tai ON tu.idApp = tai.id" +
-        " LEFT JOIN (SELECT tbluserinformation.email as 'sharedUser',tblvehicle.Name, tblvehicle.deviceid,tblvehicle.id,tblvehicle.iduser" +
-        " FROM tblvehicle LEFT JOIN tbluserinformation ON tbluserinformation.id= tblvehicle.iduser) as tv on tv.deviceid= ts.DeviceId " + search;
+        " LEFT JOIN tbluserinformation tus ON ts.idSharedUser = tus.id " +
+        " LEFT JOIN tblvehicle tv ON tv.deviceid = ts.DeviceId " + search;
+    // var Countqry = "SELECT count(ts.id) as TotalRecord " +
+    //     " FROM tblsharedevice ts  LEFT JOIN tbluserinformation tu on tu.id = ts.idUser" +
+    //     " LEFT Join tblappinfo tai ON tu.idApp = tai.id" +
+    //     " LEFT JOIN (SELECT tbluserinformation.email as 'sharedUser',tblvehicle.Name, tblvehicle.deviceid,tblvehicle.id,tblvehicle.iduser" +
+    //     " FROM tblvehicle LEFT JOIN tbluserinformation ON tbluserinformation.id= tblvehicle.iduser) as tv on tv.deviceid= ts.DeviceId " + search;
     connection.query(query, function(err, response) {
         if (response != undefined) {
             connection.query(Countqry, function(err, lstCount, fields) {
