6625e3b8cd13632053914e6d44e553f38567f358
Soham <soham.patel@bugzstudio.com>
2018-08-02 14:55:27 +0530

    remove unwanted file call from server.js

--- server.js
+++ server.js
@@ -554,7 +554,6 @@ app.use('/dynamicpage', require('./controllers/dynamicpage'));
 //socket API start
 app.use('/socketapi', require('./controllers/socketapi'));
 app.use('/socketapi_concox', require('./controllers/socketapi_concox'));
-app.use('/socketapi_gpsbox', require('./controllers/socketapi_gpsbox'));
 
 app.use('/PetDevice', require('./controllers/PetDevice'));
 
