38fbfa73ab395eb9b8f7c0ebbc81ccafaf80c019
Dino Saw <dino.saw@bugzstudio.com>
2023-05-15 17:58:29 +0800

    1. Format only.

--- controllers/address.js
+++ controllers/address.js
@@ -2,14 +2,16 @@ var router = express.Router();
 var Address = models.tbladdress;
 
 router.get('/SaveAddress', function (req, res) {
-    var obj = new Object();
-    obj.Address = req.query.Address;
-    obj.Lat = req.query.Lat;
-    obj.Lng = req.query.Lng;
-    Address.findOrCreate({ where: { Lat: obj.Lat, Lng: obj.Lng }, defaults: obj }).then(function (resAddress) {
-        res.json({ success: true, message: "Address created successfully." })
-    }).catch(function (err) {
-        res.json({ success: false, message: "Address could not created." })
+  var obj = new Object();
+  obj.Address = req.query.Address;
+  obj.Lat = req.query.Lat;
+  obj.Lng = req.query.Lng;
+  Address.findOrCreate({ where: { Lat: obj.Lat, Lng: obj.Lng }, defaults: obj })
+    .then(function (resAddress) {
+      res.json({ success: true, message: 'Address created successfully.' });
     })
+    .catch(function (err) {
+      res.json({ success: false, message: 'Address could not created.' });
+    });
 });
-module.exports = router
+module.exports = router;
